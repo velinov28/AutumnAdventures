1d25f43c6fb1e4d6bacb6b331aa9d6441d557133
EGT-DIGITAL\stiliyan.velinov <stiliyan.velinov@egt-digital.com>
2024-02-26 13:37:08 +0200

    Replacing attaching the html with creating the nodes in DOM. Move the selectors and some varibles from function solve() to the global scope
    Signed-off-by: EGT-DIGITAL\stiliyan.velinov <stiliyan.velinov@egt-digital.com>

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 window.addEventListener("load", solve);
 
+// Selectors
+
+// Section Add Autumn Event
+const addAutumnEventForm = document.querySelector("form");
+const whenTimePicker = document.getElementById("time");
+const whenDatePicker = document.getElementById("date");
+const placeInput = document.getElementById("place");
+const eventInput = document.getElementById("event-name");
+const contactsInput = document.getElementById("email");
+const addEventButton = document.getElementById("add-btn");
+
+let autumnEventState;
+
+// Section Last Check
+const checkList = document.getElementById("check-list");
+let editButton, continueButton;
+
+// Section Upcoming
+const upcomingList = document.getElementById("upcoming-list");
+let moveToFinishedButton;
+
+// Section Finished
+const finishedList = document.getElementById("finished-list");
+const clearButton = document.getElementById("clear");
+
+// Global variables
+let addList, addButtons;
+let listItem = document.createElement("li");
+listItem.classList.add("event-content");
+let article;
+
 function solve() {
-  // Selectors
-
-  // Section Add Autumn Event
-  const addAutumnEventForm = document.querySelector("form");
-  const whenTimePicker = document.getElementById("time");
-  const whenDatePicker = document.getElementById("date");
-  const placeInput = document.getElementById("place");
-  const eventInput = document.getElementById("event-name");
-  const contactsInput = document.getElementById("email");
-  const addEventButton = document.getElementById("add-btn");
-
-  let autumnEventState;
-
-  // Section Last Check
-  const checkList = document.getElementById("check-list");
-  let editButton, continueButton;
-
-  // Section Upcoming
-  const upcomingList = document.getElementById("upcoming-list");
-  let moveToFinishedButton;
-
-  // Section Finished
-  const finishedList = document.getElementById("finished-list");
-  const clearButton = document.getElementById("clear");
-
-  // Global variables
-  let addList, addButtons;
-
   // Event handler functions
   function addAutumnEventHandler() {
     autumnEventState = {
@@ -50,32 +53,24 @@ function solve() {
 
   function editLastCheckHandler() {
     _transferDataFromLastCheckToAddAutumnEvent();
+
     checkList.firstElementChild.remove();
     addEventButton.disabled = false;
   }
 
   function continueLastCheckHandler() {
-    let html;
-    checkList.firstElementChild.remove();
+    listItem.removeChild(editButton);
+    listItem.removeChild(continueButton);
 
-    addButtons = `
-        <button class="finished-btn">Move to Finished</button>
-        `;
-    html = addList.replace(`</li>`, addButtons + `</li>`);
-    upcomingList.insertAdjacentHTML("afterbegin", html);
-
-    moveToFinishedButton = document.querySelector(".finished-btn");
-
-    // Add event listener to the just displayed 'move to finished' button
-    moveToFinishedButton.addEventListener("click", moveToFinishedHandler);
+    _createAndAppendMoveToFinishedButton();
+    checkList.removeChild(listItem);
+    upcomingList.appendChild(listItem);
   }
 
   function moveToFinishedHandler() {
-    let html;
-    upcomingList.firstElementChild.remove();
-
-    html = addList;
-    finishedList.insertAdjacentHTML("afterbegin", html);
+    listItem.removeChild(moveToFinishedButton);
+    upcomingList.removeChild(listItem);
+    finishedList.appendChild(listItem);
   }
 
   function clearEventHandler() {
@@ -92,30 +87,65 @@ function solve() {
   }
 
   function _displayLastCheckSection() {
-    let html;
-    addList = `
-        <li class="event-content">
-            <article>
-                <p>Begins: ${whenDatePicker.value} at ${whenTimePicker.value}</p>
-                <p>In: ${placeInput.value} </p>
-                <p>Event: ${eventInput.value} </p>
-                <p>Contact: ${contactsInput.value} </p>
-            </article>
-        </li>
-    `;
-    addButtons = `
-        <button class="edit-btn">Edit</button>
-        <button class="continue-btn">Continue</button>
-        `;
-    html = addList.replace(`</li>`, addButtons + `</li>`);
-    checkList.insertAdjacentHTML("afterbegin", html);
+    _createAndAppendListItem();
+    _createAndAppendEditAndContinueButtons();
+  }
 
-    editButton = document.querySelector(".edit-btn");
-    continueButton = document.querySelector(".continue-btn");
+  function _createAndAppendListItem() {
+    // Removes the previous children if any
+    listItem.replaceChildren();
 
-    // Add evenet listeners to the just displayed 'edit' and 'continue' buttons
+    // Creates the elements in the DOM
+    article = document.createElement("article");
+
+    const paragraph1 = document.createElement("p");
+    paragraph1.textContent = `Begins: ${whenDatePicker.value} at: ${whenTimePicker.value}`;
+
+    const paragraph2 = document.createElement("p");
+    paragraph2.textContent = `In: ${placeInput.value}`;
+
+    const paragraph3 = document.createElement("p");
+    paragraph3.textContent = `Event: ${eventInput.value}`;
+
+    const paragraph4 = document.createElement("p");
+    paragraph4.textContent = `Contact: ${contactsInput.value}`;
+
+    // Attaches the newly created elements
+    article.appendChild(paragraph1);
+    article.appendChild(paragraph2);
+    article.appendChild(paragraph3);
+    article.appendChild(paragraph4);
+
+    listItem.appendChild(article);
+    checkList.appendChild(listItem);
+  }
+
+  function _createAndAppendEditAndContinueButtons() {
+    editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    editButton.classList.add("edit-btn");
+
+    continueButton = document.createElement("button");
+    continueButton.textContent = "Continue";
+    continueButton.classList.add("continue-btn");
+
+    listItem.appendChild(editButton);
+    listItem.appendChild(continueButton);
+
+    // Add event listeners to the just displayed 'edit' and 'continue' buttons
     editButton.addEventListener("click", editLastCheckHandler);
     continueButton.addEventListener("click", continueLastCheckHandler);
+  }
+
+  function _createAndAppendMoveToFinishedButton() {
+    moveToFinishedButton = document.createElement("button");
+    moveToFinishedButton.textContent = "Move to Finished";
+    moveToFinishedButton.classList.add("finished-btn");
+
+    listItem.appendChild(moveToFinishedButton);
+
+    // Add event listener to the just displayed 'move to finished' button
+    moveToFinishedButton.addEventListener("click", moveToFinishedHandler);
   }
 
   function _resetAddAutumnEventSection() {
